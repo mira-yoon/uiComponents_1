86c11697d3a8e49be9460d2dadac828c26b0137f
mira-yoon <amorfati823@gmail.com>
2024-10-30 16:10:23 +0900

    아코디언 (1/6) 보이는 요소만 렌더링

---
+++ src/components/accordion/data.ts
@@ -0,0 +1,38 @@
+const data = [
+  {
+    id: "6721c0efb7cb69a3e9f48fd1",
+    title:
+      "Excepteur officia consequat eu dolore consequat nostrud esse commodo culpa laboris fugiat nostrud. Ipsum duis ipsum veniam sint duis et ipsum cupidatat consequat pariatur deserunt consequat non.",
+    description:
+      "Nisi nulla consequat dolore laboris id pariatur fugiat nostrud magna reprehenderit. Reprehenderit dolor do laborum amet laborum deserunt duis veniam aliqua excepteur ut voluptate. Cillum voluptate anim dolor adipisicing ut nisi duis dolor aute officia ullamco occaecat occaecat. Ad commodo quis laborum sit veniam voluptate culpa commodo tempor laboris nostrud.\r\nEnim excepteur adipisicing est non exercitation et tempor do mollit. Minim do id minim Lorem qui elit ad. Esse dolor dolore anim exercitation enim dolore amet culpa consequat duis enim aliqua. Irure adipisicing proident anim quis esse ad tempor. Duis id et irure amet. Adipisicing magna aliqua excepteur tempor deserunt consectetur sunt.\r\n",
+  },
+  {
+    id: "6721c0ef6c3e03ace1a33ebe",
+    title:
+      "Quis ut excepteur laboris eiusmod sunt mollit dolor in proident proident aliquip.",
+    description:
+      "Nulla pariatur excepteur voluptate proident tempor ex occaecat dolor incididunt. Id duis consectetur duis consequat cillum culpa nostrud adipisicing veniam. Velit sunt ut laborum irure laborum esse magna. Proident reprehenderit nisi est occaecat quis commodo enim irure excepteur aliquip adipisicing. Anim pariatur ipsum dolore sunt duis ad sit culpa ipsum.\r\nEst occaecat mollit Lorem duis eiusmod commodo sunt reprehenderit aliqua ex ut officia. Aute irure enim exercitation nostrud laboris. Ex ex incididunt Lorem sunt.\r\nConsequat ullamco ex tempor reprehenderit do nulla sit culpa et nisi nostrud pariatur ad. Velit consequat adipisicing nisi duis laboris sint do. Et minim commodo deserunt ipsum consequat qui in deserunt exercitation culpa. Non elit officia deserunt qui esse laboris amet enim nisi amet magna tempor. Ipsum eu cupidatat anim fugiat velit exercitation. Est mollit deserunt irure aliquip veniam do officia aliquip qui non in. Lorem veniam id veniam do.\r\n",
+  },
+  {
+    id: "6721c0ef044bc9a34d653508",
+    title:
+      "Sunt magna velit labore aute adipisicing. Ut do duis enim esse enim laborum deserunt officia eiusmod mollit est aliquip.",
+    description:
+      "Consectetur in laborum non non occaecat mollit ipsum dolor nostrud. Culpa nostrud cupidatat excepteur reprehenderit Lorem culpa fugiat tempor cillum nisi officia cupidatat fugiat ipsum. Nostrud mollit sit dolor reprehenderit. Sunt deserunt dolore ullamco duis esse voluptate ad duis.\r\nLabore velit cupidatat duis Lorem aliquip aliquip aute veniam labore qui duis aliquip enim. Officia aute tempor pariatur mollit labore eu Lorem amet est incididunt amet excepteur non. Amet ad occaecat officia sint officia. Amet ad id pariatur aliquip deserunt ut ex do occaecat ut. Anim ex aute cupidatat ad reprehenderit. Nostrud amet aliqua adipisicing et in Lorem laboris laboris sit quis irure. Excepteur incididunt occaecat ipsum quis.\r\nMinim minim veniam reprehenderit minim reprehenderit in nulla. Aliqua do ullamco voluptate nulla pariatur elit. Nostrud incididunt duis aute ullamco occaecat minim do consequat.\r\nEu fugiat tempor voluptate culpa ut nisi nostrud nisi adipisicing incididunt. Cillum qui aute reprehenderit consectetur consectetur officia culpa aliquip irure ad esse proident aliqua laboris. Qui minim sit consequat deserunt dolor qui cillum in dolore. Ad proident nisi minim labore eiusmod nisi sit irure excepteur. Laboris officia officia sunt id aliqua sit aliquip nostrud occaecat. Ullamco aliquip adipisicing occaecat id eu est sit cupidatat aute consectetur duis. Velit esse esse eiusmod labore sit excepteur sint nostrud do do.\r\n",
+  },
+  {
+    id: "6721c0efdec3ab737b8f28fc",
+    title: "Anim sunt quis reprehenderit dolor.",
+    description:
+      "Qui culpa ut ut aliquip ex ipsum. Anim sit tempor pariatur non occaecat eu non et sunt dolore dolor aliquip sit minim. Excepteur magna adipisicing occaecat nisi sit labore veniam veniam deserunt irure nulla. Consequat velit et mollit do voluptate laboris commodo nostrud veniam aliqua. Et consequat qui aliquip consectetur do. Eiusmod mollit laborum esse velit non. Fugiat commodo in cupidatat fugiat labore et qui ex nulla eu aliquip.\r\nLaborum aliqua excepteur in est id ullamco sit. Aute quis et voluptate deserunt eiusmod in exercitation excepteur. Lorem mollit est amet et aliqua. Veniam eu excepteur magna enim. Labore esse sit ut anim esse sunt nostrud in reprehenderit aute nisi. Et cupidatat tempor ad laboris eiusmod dolor aute eu deserunt excepteur tempor qui id aliquip.\r\nId sunt deserunt ea voluptate deserunt reprehenderit incididunt sunt nisi magna culpa. Ipsum culpa ipsum consectetur anim ad voluptate proident quis labore eu eiusmod cillum anim. Qui magna esse fugiat tempor elit labore ullamco aute consequat enim elit Lorem magna consequat. Voluptate aute occaecat eu duis aute amet minim do non cillum Lorem pariatur exercitation ad.\r\nDolore reprehenderit ex incididunt laborum pariatur commodo duis voluptate laborum tempor. Sit amet sint elit id officia excepteur qui. Consectetur ex sit aliquip sunt sint elit duis aliqua. Ad elit tempor non ipsum in et. Irure cillum qui magna enim sit veniam consectetur. Aliqua reprehenderit esse in aute exercitation tempor exercitation dolor sint. Nisi et culpa elit irure consequat.\r\n",
+  },
+  {
+    id: "6721c0ef28f6fbe6ed982fa5",
+    title:
+      "Anim culpa dolore laborum pariatur ad duis. Ex do dolor nisi amet amet officia.",
+    description:
+      "Cillum ut non do minim ex voluptate. Ullamco eu nostrud dolor cillum dolor deserunt excepteur laboris occaecat elit sit et deserunt. Enim mollit laborum eiusmod magna culpa cillum ad anim mollit reprehenderit ea. Culpa anim cillum eu eiusmod officia do veniam ea. Ad sint tempor deserunt dolor nisi.\r\nEx quis fugiat adipisicing culpa est pariatur labore aliqua. Excepteur in amet excepteur nostrud duis officia culpa ad aliqua ut aute excepteur. Ipsum anim exercitation qui officia. Pariatur Lorem ad proident cupidatat non adipisicing duis fugiat. Nulla aliqua irure id Lorem consectetur. Laborum exercitation proident culpa adipisicing in. Aliqua velit dolor labore id ex do tempor incididunt sit commodo ad enim.\r\nMollit incididunt amet tempor nisi amet dolore duis reprehenderit in ut eu consectetur proident. Occaecat ut culpa officia ipsum et proident nulla cupidatat. Officia excepteur Lorem anim esse nulla. Pariatur pariatur non elit ut voluptate ut reprehenderit tempor excepteur ipsum. Elit amet esse ipsum ullamco aute Lorem tempor qui. Mollit fugiat occaecat pariatur excepteur ut dolore cupidatat tempor consectetur. Laboris consectetur est laborum eiusmod eiusmod nulla consequat reprehenderit ex enim non ad.\r\n",
+  },
+];
+
+export default data;
